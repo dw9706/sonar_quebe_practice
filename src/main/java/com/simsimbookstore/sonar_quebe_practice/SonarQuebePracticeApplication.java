358e5feb7ccd7a5e499163125c4ae3249a37b420
kodongwon <dw9706@naver.com>
2024-12-20 11:09:40 +0900

    t

---
+++ src/main/java/com/simsimbookstore/sonar_quebe_practice/SonarQuebePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.simsimbookstore.sonar_quebe_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQuebePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarQuebePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simsimbookstore/sonar_quebe_practice/SonarQuebePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simsimbookstore.sonar_quebe_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarQuebePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
